8f77a206e184fe6b87e9d0cd761f9ee782081bbe
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-08-18 15:57:19 +0200

    new changes

--- potifolio/src/components/Header.jsx
+++ potifolio/src/components/Header.jsx
@@ -5,11 +5,11 @@ import image from '../assets/images/sam.jpg'
 function Header() {
   return (
     <>
-    <header className='w-full flex bg-bg-input-blue-light-light text-white  text-xl items-center'>
+    <header className='w-full flex bg-bg-input-blue-light-light text-white  text-xl items-center justify-between'>
         <div className='rounded-full'><img className='w-[100px] p-5 ' src={logo} alt="" /></div>
         
             <div className=' '>
-                <ul className='flex gap-4'>
+                <ul className='flex gap-15'>
                        <li><a href="">Home</a></li>
                        <li><a href="">About</a></li>
                        <li><a href="">Skills</a></li>
@@ -17,7 +17,7 @@ function Header() {
                        <li><a href="">contact</a></li>
                 </ul>
             </div>
-            <div className='ml-auto pl-10 '> <img className="w-24 h-24 rounded-full border-2 border-blue-500 object-cover" src={image} alt="Profile" /></div>
+            <div className='m-4'> <img className="w-24 h-24 rounded-full border-2 border-blue-500 object-cover " src={image} alt="Profile" /></div>
      </header>
 
     </>
